5334c1ed209faa38a4c04b8cad5df9993d315be6
MancunianRed <proichenko@gmail.com>
2021-10-21 13:53:10 -0600

    lesson 5_3 has been updated

--- Lesson_5/Lesson_5_task_3.py
+++ Lesson_5/Lesson_5_task_3.py
@@ -1,13 +1,10 @@
 #  Циклы: Задание 3 - 40 баллов
 jewel = ['золото', 'алмазы', 'серебро', 'сапфиры', 'бронза', 'рубины',
          'платина', 'изумруды', 'палладий', 'аметисты']
-for index, value in enumerate(jewel[::2], start=1):
-    print(index, value)
-print()
-for index, value in enumerate(jewel[1::2], start=1):
-    print(index, value)
-
-
+for i in range(2):
+    for index, value in enumerate(jewel[i::2], 1):
+        print(index, value)
+    print()
 
 
 
