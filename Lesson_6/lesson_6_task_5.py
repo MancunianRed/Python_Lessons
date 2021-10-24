682b28ce98a52dbaef5d34f30a99853e9fb12849
MancunianRed <proichenko@gmail.com>
2021-10-24 14:15:02 -0600

    file lesson_6_task_5 has been reformatted

--- Lesson_6/lesson_6_task_5.py
+++ Lesson_6/lesson_6_task_5.py
@@ -5,15 +5,15 @@ arguments_list = arguments.split()
 
 def square_math(*args):
     if len(args) == 1:
-        print('Площадь круга: ', round(3.14 * int(args[0])**2 / 4, 2),
+        print('Площадь круга: ', round(3.14 * int(args[0]) ** 2 / 4, 2),
               ' кв. метров')
     elif len(args) == 2:
         print('Площадь прямоугольника: ', int(args[0]) * int(args[1]),
               ' кв. метров')
     else:
         side = (int(args[0]) + int(args[1]) + int(args[2])) / 3
-        print('Площадь треугольника: ', round((3**0.5 * side**2) / 4, 2),
-                ' кв. метров')
+        print('Площадь треугольника: ', round((3 ** 0.5 * side ** 2) / 4, 2),
+              ' кв. метров')
 
 
 square_math(*arguments_list)
