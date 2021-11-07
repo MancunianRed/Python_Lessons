31a7e2ebf59d793f4f5478da480353234213cec9
MancunianRed <proichenko@gmail.com>
2021-11-07 12:38:38 -0700

    commit 2

--- Lesson_9/lesson_9_task_2/lesson_9_task_2.py
+++ Lesson_9/lesson_9_task_2/lesson_9_task_2.py
@@ -1,17 +1,8 @@
 #  Работа с файлами: Задание 2 - 25 баллов
 
-# try:
-#     with open('city.txt', encoding='utf-8') as file:
-#         print(len([i for i in file.readlines()]))
-# except FileNotFoundError:
-#     print('File not found')
-
 try:
-    with open('../lesson_9_task_3/city.txt', encoding='utf-8') as file:
-        count = 0
-        for i in file.readlines():
-            count += 1
-        print('Количество строк в файле: ', count)
-except:
+    with open('city.txt', encoding='utf-8') as file:
+        print(len(file.readlines()))
+except FileNotFoundError:
     print('File error')
 
--- Lesson_9/lesson_9_task_3/lesson_9_task_3.py
+++ Lesson_9/lesson_9_task_3/lesson_9_task_3.py
@@ -1,11 +1,10 @@
 #  Работа с файлами: Задание 3 - 50 баллов
 
-try:
-    with open('city.txt', encoding='utf-8') as file:
-        with open('city_2.txt', 'a', encoding='utf-8') as file1:
-            for i in file.readlines():
-                if 'о' not in i:
-                    file1.write(str(i.strip()) + '\n')
-except:
-    print('File error')
+with open('city.txt', encoding='utf-8') as reader, \
+        open('city_2.txt', 'a', encoding='utf-8') as writer:
+    for i in reader.readlines():
+        if 'о' not in i:
+            writer.write(str(i.strip()) + '\n')
+
+
 
