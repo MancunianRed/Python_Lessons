630bbd01bfaa1c0e21e1dc6c34a4015a6fe1c65e
MancunianRed <proichenko@gmail.com>
2021-11-16 11:01:52 -0700

    sintax sugar added

--- Lesson_11.1/lesson_11.1_task_4/lesson_11.1_task_4.py
+++ Lesson_11.1/lesson_11.1_task_4/lesson_11.1_task_4.py
@@ -1,21 +1,21 @@
 #  Классы и ООП часть 2: Задание 4 - 100 баллов
 
 
+def my_decorator(function_to_decorate):
+    def wrap(*args):
+        string_1 = '+' + '-+' * len(*args)
+        string_2 = '|' + '|'.join(*args) + '|'
+        print(f'{string_1}\n{string_2}\n{string_1}')
+        return function_to_decorate
+    return wrap
+
+
+@my_decorator
 class TextFormat:
     def __init__(self, word):
         self.word = word
         print(self.word)
 
 
-def my_decorator(function_to_decorate):
-    def wrap():
-        string_1 = '+' + '-+' * len(function_to_decorate.word)
-        string_2 = '|' + '|'.join(function_to_decorate.word) + '|'
-        print(f'{string_1}\n{string_2}\n{string_1}')
-    return wrap
-
-
-if __name__ == '__main':
-    new_function = my_decorator(TextFormat('Hello'))
-    new_function()
-
+if __name__ == '__main__':
+    TextFormat('Hello')
