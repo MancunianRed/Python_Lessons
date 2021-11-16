d632c0072d1dfcf75db562113a13cd558a52c22a
MancunianRed <proichenko@gmail.com>
2021-11-16 11:14:19 -0700

    sintax sugar added

--- Lesson_11.1/lesson_11.1_task_5/lesson_11.1_task_5.py
+++ Lesson_11.1/lesson_11.1_task_5/lesson_11.1_task_5.py
@@ -1,21 +1,24 @@
 #  Классы и ООП часть 2: Задание 5 - 100 баллов
-import datetime
-
-
-class Hello:
-    print("Hello")
 
 
 def my_decorator(function_to_decorate):
-    def wrap():
-        function_to_decorate()
+    import datetime
+
+    def wrap(*args):
+        function_to_decorate(*args)
         current_date = datetime.datetime.now()
         current_date_string = current_date.strftime('%d-%m-%y %H:%M:%S')
         print(current_date_string)
     return wrap
 
 
-if __name__ == '__main':
-    new_function = my_decorator(Hello)
-    new_function()
+@my_decorator
+class Hello:
+    def __init__(self, a):
+        self.a = a
+        print(self.a)
+
+
+if __name__ == '__main__':
+    Hello('Hello')
 
