c790ee7a97342ad3fd5592d83a8b46024bb47cfb
MancunianRed <proichenko@gmail.com>
2021-12-08 00:48:26 -0700

    regex has been changed in lesson 1 and 3

--- Part_2/Lesson_4.1/lesson_4.1_task_1/lesson_4.1_task_1.py
+++ Part_2/Lesson_4.1/lesson_4.1_task_1/lesson_4.1_task_1.py
@@ -4,6 +4,6 @@ import re
 text = 'Сначала был адрес http://yandex.ru, потом стал https://yandex.ru.' \
        ' \ Гугл https://google.com имеет шире охват чем https://yandex.ru.'
 
-res = re.findall(r'(https?://[\w]+\.[a-z]+)', text)
+res = re.findall(r'(https?://\w+\.[a-z]+)', text)
 for url in sorted(set(res)):
     print(''.join(url))
--- Part_2/Lesson_4.1/lesson_4.1_task_3/lesson_4.1_task_3.py
+++ Part_2/Lesson_4.1/lesson_4.1_task_3/lesson_4.1_task_3.py
@@ -5,7 +5,7 @@ with open('surnames.txt', encoding='utf-8') as reader, \
     open('woman.txt', 'a', encoding='utf-8') as writer_woman, \
     open('man.txt', 'a', encoding='utf-8') as writer_man:
     for name in reader:
-        if (res := re.match(r'.+АЯ|.+ВА|.+НА', name)) is not None:
+        if (res := re.match(r'.+АЯ$|.+ВА$|.+НА$', name)) is not None:
             writer_woman.write(res.group().strip().title() + '\n')
         else:
             writer_man.write(name.strip().title() + '\n')
