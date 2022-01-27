3c1f7e27a408c5c518870002fa4607dc08fa29c9
MancunianRed <proichenko@gmail.com>
2022-01-27 14:06:03 -0700

    update task1

--- Part_2/Lesson_14/lesson_14_task_1/lesson_14_task_1.py
+++ Part_2/Lesson_14/lesson_14_task_1/lesson_14_task_1.py
@@ -1,37 +1,18 @@
 #  Криптография: Задание 1 - 50 баллов
-import string
+
 
 def input_messages():
     value = input("Select action: ")
     message = input("Enter a message: ").upper()
     if value == "1":
-        encode(message)
-    else:
-        decode(message)
+        print(f"Encrypted message: {encrypt_decrypt(message)}")
+    elif value == "2":
+        print(f"Decrypted message: {encrypt_decrypt(message)}")
 
 
-def encode(message):
-    encode_word = ""
-    alphabet = [chr(x) for x in range(1040, 1072)]
-    alpha = list(alphabet)
-    alpha.reverse()
-    for symbol in message:
-        for index, value in enumerate(alphabet):
-            if symbol == value:
-                encode_word += alpha[index]
-    print(f"Encrypted message: {encode_word}")
-
-
-def decode(message):
-    decode_word = ""
-    alphabet = [chr(x) for x in range(1040, 1072)]
-    alpha = list(alphabet)
-    alpha.reverse()
-    for symbol in message:
-        for index, value in enumerate(alpha):
-            if symbol == value:
-                decode_word += alphabet[index]
-    print(f"Decrypted message: {decode_word}")
+def encrypt_decrypt(message):
+    alphabet = "".join([chr(x) for x in range(1040, 1072)])
+    return message.translate(str.maketrans(alphabet, alphabet[::-1]))
 
 
 if __name__ == "__main__":
