0a1bdb69dc7f69c2d4ed364649a4a029a2d5ea4d
MancunianRed <proichenko@gmail.com>
2022-01-29 21:44:08 -0700

    task 2 has been updated with list generator and tags_print function

--- Part_2/Lesson_15/lesson_15_task_1/lesson_15_task_1.py
+++ Part_2/Lesson_15/lesson_15_task_1/lesson_15_task_1.py
@@ -4,30 +4,32 @@ import exifread
 import os
 import re
 
+filters = "JPEGThumbnail Thumbnail JPEGInterchangeFormatLength " \
+          "Interoperability InteroperabilityIndex " \
+          "Interoperability InteroperabilityVersion " \
+          "Thumbnail JPEGInterchangeFormat"
+
 
 def get_files_from_dir(path):
-    files_list = []
-    with os.scandir(path) as i:
-        for f in i:
-            if f.is_file() and re.match(r".+\.((jpg)|(JPG))", f.name):
-                files_list.append(f.name)
+    with os.scandir(path) as scan:
+        files_list = [f.name for f in scan if f.is_file() and
+                      re.match(r".+\.((jpg)|(JPG))", f.name)]
     return files_list, path
 
 
 def meta_data_jpg(data):
     for file in data[0]:
         with open(f"{data[1]}\{file}", "rb") as reader:
-            tags = exifread.process_file(reader)
-            if (str(tags["Image DateTime"]).split(":")[0] == "2019") and (str(
-                    tags["Image DateTime"]).split(":")[1] == "09"):
-                for tag in tags:
-                    if re.match(r"EXIF.+", tag) is None and tag not in (
-                            "JPEGThumbnail",
-                            "Thumbnail JPEGInterchangeFormatLength",
-                            "Interoperability InteroperabilityIndex",
-                            "Interoperability InteroperabilityVersion",
-                            "Thumbnail JPEGInterchangeFormat"):
-                        print(f"{tag:<25} : {tags[tag]}")
+            tags_print(reader)
+
+
+def tags_print(reader):
+    tags = exifread.process_file(reader)
+    if (str(tags["Image DateTime"]).split(":")[0] == "2019") and (str(
+            tags["Image DateTime"]).split(":")[1] == "09"):
+        for tag in tags:
+            if re.match(r"EXIF.+", tag) is None and tag not in filters:
+                print(f"{tag:<25} : {tags[tag]}")
 
 
 if __name__ == "__main__":
